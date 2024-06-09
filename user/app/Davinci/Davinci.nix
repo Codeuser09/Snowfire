47ea89f01c48490c77a5c6ed869c6f224baae2d4
Simon <simon.schober@outlook.de>
2024-06-09 11:40:06 +0200

    Changed shit

---
+++ user/app/Davinci/Davinci.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  # Module installing brave as default browser
+  home.packages = with pkgs; [
+    davinci-resolve
+    librsvg
+    libGLU
+    libGL
+    xorg.libICE
+    xorg.libSM
+    xorg.libXxf86vm
+    xorg.libxcb
+    udev
+    opencl-headers
+    alsa-lib
+    xorg.libX11
+    xorg.libXext
+    expat
+    zlib
+    libuuid
+    bzip2
+    libtool
+    ocl-icd
+    glib
+    libarchive
+    xdg-utils # xdg-open needed to open URLs
+    python
+    dbus
+    ];
+}
