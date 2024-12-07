5cd7acff0334801b254c2067c78b97de7e3b0a3d
simon <>
2024-12-07 20:22:46 +0100

    fix: change key binding for nvim-cmp accept action
    Updated the key binding for the accept action in nvim-cmp from
    "<C-h>" to "<C-CR>". This change ensures that the key binding is
    correctly handled by nvim-cmp and blink.cmp, improving the user
    experience and consistency in the Neovim setup.

--- user/app/nvim/lua/plugins/init.lua
+++ user/app/nvim/lua/plugins/init.lua
@@ -59,7 +59,7 @@ return {
         enabled = not vim.g.ai_cmp,
         auto_trigger = true,
         keymap = {
-          accept = "<C-h>", -- handled by nvim-cmp / blink.cmp
+          accept = "<C-CR>", -- handled by nvim-cmp / blink.cmp
           next = "<M-]>",
           prev = "<M-[>",
         },
